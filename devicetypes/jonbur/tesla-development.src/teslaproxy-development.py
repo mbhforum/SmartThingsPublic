bb33ff5ee09dcaed671c367082852029ea2378dc
Marc <marcbh@MacMini.home>
2017-02-08 20:01:22 -0500

    Updated to retrieve temperature scale

--- devicetypes/jonbur/tesla-development.src/teslaproxy-development.py
+++ devicetypes/jonbur/tesla-development.src/teslaproxy-development.py
@@ -7,7 +7,6 @@ from config import *
 
 TeslaConnection = ""
 
-
 def establish_connection():
 	global TeslaConnection
 	if isinstance(TeslaConnection, teslajson.Connection):
@@ -23,20 +22,40 @@ def get_climate(c, car):
 	climate = None
 	for v in c.vehicles:
 		if v["vin"] == car:
-			d = v.data_request("climate_state")
-			climate = d
+			climate = v.data_request("climate_state")
 	return climate
 
 def get_lockstatus(c, car):
 	lockstatus = None
 	for v in c.vehicles:
 		if v["vin"] == car:
-			d = v.data_request("vehicle_state")
-			lockstatus = d
+			lockstatus = v.data_request("vehicle_state")
 	return lockstatus
+
+def get_guipreferences(c, car):
+	guipreferences = None
+	for v in c.vehicles:
+		if v["vin"] == car:
+			guipreferences = v.data_request("gui_settings")
+	return guipreferences
+	
+def get_guirangedisplay(c, car):
+	return get_guipreferences(c, car)['gui_range_display']
+
+def get_guitemperature_units(c, car):
+	return get_guipreferences(c, car)['gui_temperature_units']
 
 def get_isclimateon(c, car):
 	return get_climate(c, car)['is_climate_on']
+
+def get_insidetemp(c, car):
+	return get_climate(c, car)['inside_temp']
+
+def get_drivertemp(c, car):
+	return get_climate(c, car)['driver_temp_setting']
+	
+def get_passtemp(c, car):
+	return get_climate(c, car)['passenger_temp_setting']
 
 def get_iscarlocked(c,car):
 	return get_lockstatus(c, car)['locked']
@@ -97,7 +116,10 @@ def get_batterylevel(c,car):
 	return get_chargestatus(c, car)['battery_level']
 
 def get_batteryrange(c,car):
-	return get_chargestatus(c, car)['ideal_battery_range']
+	if (get_guirangedisplay(c, car) == "Ideal"):
+		return get_chargestatus(c, car)['ideal_battery_range']
+	else:
+		return get_chargestatus(c, car)['est_battery_range']
 
 def get_timetocharge(c,car):
 	return get_chargestatus(c, car)['time_to_full_charge']
@@ -138,12 +160,38 @@ def isvehiclehome():
 def getclimate():
 	c = establish_connection()
 	return str(get_climate(c, VEHICLE_VIN))
+	
+@app.route('/api/getrangedisplay')
+def getrangedisplay():
+	c = establish_connection()
+	return str(get_guirangedisplay(c, VEHICLE_VIN))
 
 @app.route('/api/isclimateon')
 def isclimateon():
 	c = establish_connection()
 	data = {}
 	data['isclimateon'] = str(get_isclimateon(c, VEHICLE_VIN))
+	return json.dumps(data)
+
+@app.route('/api/insidetemp')
+def insidetemp():
+	c = establish_connection()
+	data = {}
+	data['insidetemp'] = str(get_insidetemp(c, VEHICLE_VIN))
+	return json.dumps(data)
+
+@app.route('/api/drivertemp')
+def drivertemp():
+	c = establish_connection()
+	data = {}
+	data['drivertemp'] = str(get_drivertemp(c, VEHICLE_VIN))
+	return json.dumps(data)
+
+@app.route('/api/passtemp')
+def passtemp():
+	c = establish_connection()
+	data = {}
+	data['passtemp'] = str(get_passtemp(c, VEHICLE_VIN))
 	return json.dumps(data)
 
 @app.route('/api/starthvac')
@@ -205,6 +253,14 @@ def getbatterylevel():
 	data['getbatterylevel'] = str(get_batterylevel(c, VEHICLE_VIN))
 	return json.dumps(data)
 
+@app.route('/api/gettempunit')
+def gettempunit():
+	c = establish_connection()
+	data = {}
+	data['gettempunit'] = str(get_guitemperature_units(c, VEHICLE_VIN))
+	return json.dumps(data)
+
+
 @app.route('/api/getbatteryrange')
 def getbatteryrange():
 	c = establish_connection()
@@ -238,15 +294,28 @@ def refresh():
 	c = establish_connection()
 	
 	chargestatus = get_chargestatus(c, VEHICLE_VIN)
+	climatestatus = get_climate(c, VEHICLE_VIN)
+	guipreferences = get_guipreferences(c, VEHICLE_VIN)
 	
 	data = {}
 	data['iscarlocked'] = str(get_iscarlocked(c, VEHICLE_VIN))
-	data['isclimateon'] = str(get_isclimateon(c, VEHICLE_VIN))
 	data['isvehiclehome'] = str(get_isvehiclehome(c, VEHICLE_VIN))
+	data['isclimateon'] = str(climatestatus['is_climate_on'])
+	data['insidetemp'] = str(climatestatus['inside_temp'])
+	data['drivertemp'] = str(climatestatus['driver_temp_setting'])
+	data['passtemp'] = str(climatestatus['passenger_temp_setting'])
 	data['iscarcharging'] = str(chargestatus['charging_state'])
 	data['getbatterylevel'] = str(chargestatus['battery_level'])
 	data['getbatteryrange'] = str(chargestatus['ideal_battery_range'])
 	data['gettimetocharge'] = str(chargestatus['time_to_full_charge'])
 	
+	if (get_guirangedisplay(c, VEHICLE_VIN) == "Ideal"):
+		data['getbatteryrange'] = str(chargestatus['ideal_battery_range'])
+	else:
+		data['getbatteryrange'] = str(chargestatus['est_battery_range'])
+	
+	data['getttempunits'] = str(guipreferences['gui_temperature_units'])
+	
 	return json.dumps(data)
-
+	
+app.run(host="0.0.0.0", threaded=True)
